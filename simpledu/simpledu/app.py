4abeb4ef34321c8a8fa3b682d185c113ce065127
Lemon-mmg <mamingguo.love@163.com>
2019-04-26 20:58:45 +0800

    ceshi

---
+++ simpledu/simpledu/app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template
+from simpledu.config import configs
+from simpledu.models import db, Course
+
+def register_blueprints(app):
+    from .handlers import front, course, admin,user
+    app.register_blueprint(front)
+    app.register_blueprint(course)
+    app.register_blueprint(admin)
+    app.register_blueprint(user)
+
+def create_app(config):
+    app = Flask(__name__)
+    app.config.from_object(configs.get(config))
+    db.init_app(app)
+    register_blueprints(app)
+
+    @app.errorhandler(404)
+    def not_found(e):
+        return 'Not Found'
+
+    return app
+
